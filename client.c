ca4f959614d6e27d961dccd5783f0058a9139a10
Otxobot <otxoboy64@gmail.com>
2023-02-26 13:32:53 +0100

    headers left

---
+++ client.c
@@ -0,0 +1,61 @@
+#include "minitalk.h"
+
+static void	sig_handler(int sig)
+{
+	static int	received = 0;
+
+	if (sig == SIGUSR1)
+		++received;
+	else
+	{
+		ft_putnbr_fd(received, 1);
+		ft_putchar_fd('\n', 1);
+		exit(0);
+	}
+}
+
+static void	get_bits(int pid, char *str)
+{
+	int		i;
+	char	c;
+
+	while (*str != '\0')
+	{
+		i = 8;
+		c = *str++;
+		while (i--)
+		{
+			if (c >> i & 1)
+				kill(pid, SIGUSR2);
+			else
+				kill(pid, SIGUSR1);
+			usleep(100);
+		}
+	}
+	i = 8;
+	while (i--)
+	{
+		kill(pid, SIGUSR1);
+		usleep(100);
+	}
+}
+
+int	main(int ac, char **av)
+{
+	if (ac != 3 || !ft_strlen(av[2]))
+	{
+		ft_putstr_fd("Wrong format: <PID> <MESSAGE>", 1);
+		ft_putchar_fd('\n', 1);
+		return (1);
+	}
+	ft_putstr_fd("Sent    : ", 1);
+	ft_putnbr_fd(ft_strlen(av[2]), 1);
+	ft_putchar_fd('\n', 1);
+	ft_putstr_fd("Received: ", 1);
+	signal(SIGUSR1, sig_handler);
+	signal(SIGUSR2, sig_handler);
+	get_bits(ft_atoi(av[1]), av[2]);
+	while (1)
+		pause();
+	return (0);
+}
---
+++ minitalk.h
@@ -0,0 +1,13 @@
+#ifndef MINITALK_H
+# define MINITALK_H
+
+# include <signal.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include "libft/libft.h"
+
+# define TRUE SIGUSR1
+# define FALSE SIGUSR2
+
+#endif
